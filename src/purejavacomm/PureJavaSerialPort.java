8af9ef33f050441aa27527aaf5e16a0bcdce5d1a
reapzor <reapzor@gmail.com>
2016-01-03 11:40:49 -0500

    send closed port event unconditionally (but only once)

--- src/purejavacomm/PureJavaSerialPort.java
+++ src/purejavacomm/PureJavaSerialPort.java
@@ -1088,8 +1088,8 @@ public class PureJavaSerialPort extends SerialPort {
 				}
 			}
 			super.close();
-			sendClosedPortEvents();
 		}
+		sendClosedPortEvents();
 	}
 
 	/* package */PureJavaSerialPort(String name, int timeout) throws PortInUseException {
